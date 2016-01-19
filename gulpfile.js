928c7e3958b8e2e91eb033cb36d37f0127c03199
Jason Tham <jtham@sbcglobal.net>
2016-01-19 11:02:07 -0600

    added basic boilerplate functionality

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,15 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
+var watch = require('gulp-watch');
+watch(['./source/*.js'], function(){
+  console.log("App has been changed, re-compiling with gulp");
+  gulp.start('default');
+});
+
 gulp.task('default', function(){
   console.log("I am the default task");
-  return browserify.('./source/app.js')
+  return browserify('./source/app.js')
           .transform("babelify", {presets: ["es2015", "react"]})
           .bundle()
           .pipe(source('build.js'))
--- source/app.js
+++ source/app.js
@@ -1,4 +1,18 @@
-var app = app || {};
-app.hey = function(){
-  console.log("Hey");
+
+class HelloWorld {
+  constructor(name, purpose) {
+    this.attrs = {
+      name: name,
+      purpose: purpose
+    }
+  }
+  // define a toString() method on the class
+  toString() {
+    return 'Hello, world!';
+  }
 }
+// instantiate a new instance of HelloWorld
+var sample = new HelloWorld("Thambone", "Eat all the things");
+// console.log sample's toString() method
+console.log(sample.toString());
+console.log(sample.attrs);
